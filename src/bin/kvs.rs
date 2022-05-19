075243a117aa4659a575a94721b5258490214b51
Lind Xiao <xld704@gmail.com>
2022-05-19 18:35:05 -0400

    add

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,4 +1,6 @@
-use clap::{App, AppSettings, Arg, SubCommand};
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 use std::process::exit;
 
 fn main() {
@@ -6,31 +8,23 @@ fn main() {
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(
-                    Arg::with_name("VALUE")
-                        .help("The string value of the key")
-                        .required(true),
-                ),
-        )
         .subcommand(
             SubCommand::with_name("get")
                 .about("Get the string value of a given string key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+                .arg(Arg::with_name("key").help("A string key")),
+        )
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::with_name("key").help("A string key"))
+                .arg(Arg::with_name("value").help("The string value of the key")),
         )
         .subcommand(
             SubCommand::with_name("rm")
                 .about("Remove a given key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+                .arg(Arg::with_name("key").help("A string key")),
         )
         .get_matches();
-
     match matches.subcommand() {
         ("set", Some(_matches)) => {
             eprintln!("unimplemented");
