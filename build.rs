e2e3d57114f8358b2c38047e9962ddfa1be7443e
Lind Xiao <xld704@gmail.com>
2022-06-01 19:02:51 -0400

    generate proto code for kvs

--- build.rs
+++ build.rs
@@ -2,7 +2,11 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
- 
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    tonic_build::configure()
+        .out_dir(out_dir)
+        .compile(&["proto/kvs.proto"], &["proto"])
+        .unwrap();
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .out_dir(out_dir)
