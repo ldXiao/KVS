a8b1aac5ff5774e5f0d3228f0f0fe3492fc63c98
Lind Xiao <xld704@gmail.com>
2022-06-02 16:48:47 -0400

    Add range operation for sled engine

--- src/backend/kvsled.rs
+++ src/backend/kvsled.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, str::from_utf8};
+use std::{ops::RangeBounds, path::PathBuf, str::from_utf8};
 
 use crate::*;
 
@@ -46,5 +46,51 @@ impl KvsEngine for KvSled {
             Ok(None) => Err(KvError::KeyNotFound),
             Err(_) => Err(KvError::Unknown),
         }
+    }
+    fn range_last(&self, range: impl RangeBounds<String>) -> Result<Option<(String, String)>> {
+        match self.db.range(range).last() {
+            Some(Ok((k, v))) => Ok(Some((
+                from_utf8(k.to_vec().as_ref()).unwrap().to_string(),
+                from_utf8(v.to_vec().as_ref()).unwrap().to_string(),
+            ))),
+            Some(Err(e)) => Err(KvError::StringError(e.to_string())),
+            None => Ok(None),
+        }
+    }
+    fn range_erase(&self, range: impl RangeBounds<String>) -> Result<()> {
+        let keys: Vec<String> = self
+            .db
+            .range(range)
+            .map(|v| v.unwrap())
+            .map(|(key, _cmd)| String::from_utf8(key.to_vec()).unwrap())
+            .collect();
+        for k in keys.into_iter() {
+            self.remove(k)?;
+        }
+        Ok(())
+    }
+
+    fn export(&self) -> Result<(Vec<String>, Vec<String>)> {
+        let mut keys = Vec::new();
+        let mut values = Vec::new();
+        self.db
+            .iter()
+            .map(|entity| {
+                let (key, value) = entity.unwrap();
+                keys.push(String::from_utf8(key.to_vec()).unwrap());
+                values.push(String::from_utf8(value.to_vec()).unwrap());
+            })
+            .for_each(drop);
+        Ok((keys, values))
+    }
+    fn import(&self, data: (Vec<String>, Vec<String>)) -> Result<()> {
+        self.db.clear().unwrap();
+        let (keys, values) = data;
+        keys.into_iter()
+            .zip(values.into_iter())
+            .map(|(key, value)| self.db.insert(key.as_bytes(), value.as_bytes()))
+            .for_each(drop);
+        self.db.flush().unwrap();
+        Ok(())
     }
 }
